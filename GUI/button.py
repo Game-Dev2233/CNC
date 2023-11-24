badf65617d669d0094a7db3bb1cc89e474311824
Game Dev <owen.winchell@gmail.com>
2023-11-23 19:03:36 -0500

    Made the GUI code look better

---
+++ GUI/button.py
@@ -0,0 +1,38 @@
+import subprocess
+import tkinter as tk
+import threading
+
+root = tk.Tk()
+root.geometry("500x150")
+root.title("CNC GUI")
+
+def estop():
+    exit()
+
+def on_button_click():
+    try:
+        # run Main.py in a separate thread
+        threading.Thread(target=subprocess.run, args=(["Python", "Main.py"],)).start()
+    except Exception as e:
+        label.config(text=f"Error: {str(e)}")
+
+def setup():
+    # Create the Start CNC machine button
+    button_start = tk.Button(root, text="Start CNC Machine", command=on_button_click)
+    button_start.pack(pady=10)
+
+    # Warning label
+    global label  # Make label global so it can be accessed in the exception block
+    label = tk.Label(root, text="Warning do not touch the CNC machine insides while running the CNC machine")
+    label.pack(pady=10)
+
+    # Emergency Shutdown button
+    button_estop = tk.Button(root, text="Emergency Stop", command=estop)
+    button_estop.pack(pady=10)
+
+def es():
+    root.protocol("WM_DELETE_WINDOW", root.destroy)
+    root.mainloop()
+
+# Call the setup function
+setup()
